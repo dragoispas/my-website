adb3903dadc5ad674b4c80f7a94f513e8ad332c1
Dragos <drago.ispas@gmail.com>
2026-04-19 09:58:22 +0300

    Added education section and added a new footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { ThemeRail } from "./Components/ThereRail";
 import HeroSection from "./Sections/HeroSection";
 import AboutSection from "./Sections/AboutSection";
 import ExperienceSection from "./Sections/ExperienceSection";
+import EducationSection from "./Sections/EducationSection";
+import Separator from "./Components/Separator";
 import FooterSection from "./Sections/FooterSection";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <ThemeRail/>
       <AboutSection/>
       <ExperienceSection/>
+      <EducationSection/>
+      <Separator/>
       <FooterSection/>
     </>
   )
---
+++ src/Components/Separator.tsx
@@ -0,0 +1,7 @@
+export default function Separator() {
+  return (
+      <div className="theme-rail">
+    
+      </div>
+  )
+}
